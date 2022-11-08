b7a02423084208fbeca890e33da98814d316f6c7
elias <eliasejump@gmail.com>
2022-11-08 15:17:53 +0300

    add class PasswordGenerator

---
+++ src/PasswordGenerator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Elias\PasswordGenerator;
+
+class PasswordGenerator
+{
+    public function run(int $length = 6): string
+    {
+        $password = [];
+        $data = [
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+        ];
+
+        for ($i = 0; $i < $length; $i++) {
+            $password[]= $data[random_int(0, count($data) - 1)];
+        }
+
+        return implode('', $password);
+    }
+}
